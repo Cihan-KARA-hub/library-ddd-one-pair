70cb13e138c44b83a535a43a06e7f9d2a8b192d7
Cihan-KARA-hub <cihankara632@gmail.com>
2025-10-04 22:33:01 +0300

    feat: init project.

---
+++ src/test/java/com/turkcell/library_ddd/LibraryDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.library_ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
